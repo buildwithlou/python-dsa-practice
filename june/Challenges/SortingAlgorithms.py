c2ea40bb2254e3b8e53704e1e2d1c2d0b31a4ea0
buildwithlou <buildwithlou@gmail.com>
2026-06-02 17:23:40 -0400

    Sorting Algorithms Challenge

---
+++ june/Challenges/SortingAlgorithms.py
@@ -0,0 +1,23 @@
+students = [
+    {"name": "Carlos", "grade": 95},
+    {"name": "Maria",  "grade": 78},
+    {"name": "Ana",    "grade": 88},
+    {"name": "Bob",    "grade": 72},
+    {"name": "Eve",    "grade": 91},
+]
+
+
+
+def bubble_sort(students):
+    arr = students[:]
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - 1 - i):
+            if arr[j]['grade']> arr[j + 1]['grade']:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return [(s['name'], s['grade']) for s in arr]
+print(bubble_sort(students))
