11c7f55af6a08e0263f9ca5c9f5349f7447f326d
buildwithlou <buildwithlou@gmail.com>
2026-06-13 14:28:19 -0400

    Adding Database Integration with SQLModel

---
+++ june/Challenges/june_8/database.py
@@ -0,0 +1,20 @@
+from sqlmodel import SQLModel, Field, create_engine, Session
+from typing import Optional 
+
+#Database model - like your Pydantic model but also create a DB Table
+class Task(SQLModel, table = True):
+    id: Optional[int] = Field(default=None, primary_key= True)
+    title: str
+    status: str = "todo"
+    description: Optional[str] = None
+
+#SQLite database - stored in a file, no server needed
+DATABASE_URL = "sqlite:///tasks.db"
+engine = create_engine(DATABASE_URL)
+
+def create_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
