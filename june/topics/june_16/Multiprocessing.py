a0fa547f39059f9b1f1f21aded7eb95130f0a82b
buildwithlou <buildwithlou@gmail.com>
2026-06-16 21:46:42 -0400

    Multiprocessing Basic Topic

---
+++ june/topics/june_16/Multiprocessing.py
@@ -0,0 +1,25 @@
+from multiprocessing import Process
+import time
+import os
+
+def worker(name):
+    print(f"Process {name} started - PID: {os.getpid()}")
+    time.sleep(2)
+    print(f"Process {name} finished")
+
+if __name__ == "__main__":
+    print(f"Main process PID: {os.getpid()}")
+
+    #Creating two processes
+    p1 = Process(target=worker, args= ("A",))
+    p2 = Process(target=worker, args= ("B",))
+
+    #Start them
+    p1.start()
+    p2.start()
+
+    #Wait for both to finish
+    p1.join()
+    p2.join()
+
+    print("All processes done!")
