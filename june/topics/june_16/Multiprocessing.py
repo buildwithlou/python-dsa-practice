a0abfa0d02467a307b2d75c19ee85fdfbd8d931b
buildwithlou <buildwithlou@gmail.com>
2026-06-16 22:02:59 -0400

    Multiprocessing Real Topic

---
+++ june/topics/june_16/Multiprocessing.py
@@ -0,0 +1,22 @@
+from multiprocessing import Pool
+from pathlib import Path
+import re
+
+def analyze_log(filepath):
+    """Count errors in a single log file"""
+    errors = 0
+    with open(filepath) as f:
+        for line in f:
+            if re.search(r"ERROR",line):
+                errors += 1
+    return{"file":filepath, "errors":errors}
+
+if __name__ == "__main__":
+    #Imagine you have 100 log files to analyze
+    # log_files = ["sample.log", "sample.log", "sample.log"] #using the same file 3x
+
+    with Pool(processes=3) as pool:
+        results = pool.map(analyze_log, log_files)
+
+    for result in results:
+        print(f"{result['file']}: {result['errors']} errors")
