b8e214c0c9a9df1c0a653ea1318578b1afab8f90
buildwithlou <buildwithlou@gmail.com>
2026-06-18 19:55:36 -0400

    Asynchrony Coroutine

---
+++ june/topics/june_17/AsynchronyCoroutine.py
@@ -0,0 +1,15 @@
+import asyncio
+#async def - this is a coroutine, not a regular function
+async def greet(name, delay):
+    await asyncio.sleep(delay)  #pause without blocking
+    print(f"Hello {name}!")
+
+async def main():
+    #run all there at the same time
+    await asyncio.gather(
+        greet("Carlos", 3),
+        greet("Maria",  1),
+        greet("Ana",    2)
+    )
+
+asyncio.run(main())
