05f63d626f5d8f3f3ea2a884820d8cc852ddbe13
buildwithlou <buildwithlou@gmail.com>
2026-06-22 22:45:22 -0400

    Threading Gil Topic

---
+++ june/topics/june_22/ThreadingGil.py
@@ -0,0 +1,15 @@
+import threading
+import time
+def io_task():
+    print("Waiting for I/O...")
+    time.sleep(2)
+    print("Done!")
+
+start = time.time()
+t1 = threading.Thread(target=io_task)
+t2 = threading.Thread(target=io_task)
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print(f"Time: {time.time() - start:.2f}s")
