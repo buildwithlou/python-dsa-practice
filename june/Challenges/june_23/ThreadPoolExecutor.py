0683ca7e83b3ecf25138a51cf2af3207a572924f
buildwithlou <buildwithlou@gmail.com>
2026-06-23 22:58:25 -0400

    Thread Pool Executor Topic

---
+++ june/Challenges/june_23/ThreadPoolExecutor.py
@@ -0,0 +1,28 @@
+from concurrent.futures import ThreadPoolExecutor
+import time
+import requests
+
+def fetch_url(url):
+    response = requests.get(url)
+    return f"{url}: {response.status_code}"
+
+urls = [
+    "https://api.github.com",
+    "https://httpbin.org/get",
+    "https://jsonplaceholder.typicode.com/todos/1",
+    "https://api.github.com/users/buildwithlou"
+]
+
+#Sequential
+start = time.time()
+results = [fetch_url(url) for url in urls]
+print(f"Sequential:{time.time() - start:.2f}s")
+
+#Threaded
+start = time.time()
+with ThreadPoolExecutor(max_workers=4) as executor:
+    results = list(executor.map(fetch_url, urls))
+print(f"Threaded: {time.time() - start:.2f}s")
+
+for r in results:
+    print(r)
